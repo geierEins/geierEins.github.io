246e5ce59036a90ebc2458ddc843089d2caf843b
geierEins <58027978+geierEins@users.noreply.github.com>
2024-07-22 11:29:22 +0200

    Update stats.js

--- stats.js
+++ stats.js
@@ -1,25 +1,39 @@
 const stats = [
-    {i: 0, text: "Punkte pro Spiel"},
-    {i: 1, text: "Rebounds pro Spiel"},
-    {i: 2, text: "Steals pro Spiel"},
-    {i: 3, text: "Assists pro Spiel"},
-    {i: 4, text: "Blocks pro Spiel"},
-    {i: 5, text: "Anzahl Career Games"},
-    {i: 6, text: "Anzahl Allstar-Teilnahmen"},
-    {i: 7, text: "Free Throw Percentage"},
-    {i: 8, text: "Field Goal Percentage"},
-    {i: 9, text: "3-Punkte Percentage"},
-    {i: 10, text: "Meiste Punkte in einem Spiel"},
-    {i: 11, text: "Meiste Rebounds in einem Spiel"},
-    {i: 12, text: "Meiste Assists in einem Spiel"},
-    {i: 13, text: "Meiste Steals in einem Spiel"},
-    {i: 14, text: "Meiste Blocks in einem Spiel"},
-    {i: 15, text: "MVP-Titel + All NBA Team Erscheinungen"},
-    {i: 16, text: "DPOY-Titel + All Defensive Team Erscheinungen"},
-    {i: 17, text: "Punkte pro Spiel Karriereschnitt"},
-    {i: 18, text: "Rebounds pro Spiel Karriereschnitt"},
-    {i: 19, text: "Assists pro Spiel Karriereschnitt"},
-    {i: 20, text: "Höchste Rückennummer"}
+    
+    // beste Saison der Karriere
+    {i: 0, text: "Punkte pro Spiel (beste Saison der Karriere)"},
+    {i: 1, text: "Rebounds pro Spiel (beste Saison der Karriere)"},
+    {i: 2, text: "Steals pro Spiel (beste Saison der Karriere)"},
+    {i: 3, text: "Assists pro Spiel (beste Saison der Karriere)"},
+    {i: 4, text: "Blocks pro Spiel (beste Saison der Karriere)"},
+    {i: 5, text: "Free Throw Percentage (beste Saison der Karriere)"},
+    {i: 6, text: "Field Goal Percentage (beste Saison der Karriere)"},
+    {i: 7, text: "3-Punkte Percentage (beste Saison der Karriere)"},
+
+    // Karrierebestwerte
+    {i: 8, text: "Meiste Punkte in einem Spiel (Karrierebestwert)"},
+    {i: 9, text: "Meiste Rebounds in einem Spiel (Karrierebestwert)"},
+    {i: 10, text: "Meiste Assists in einem Spiel (Karrierebestwert)"},
+    {i: 11, text: "Meiste Steals in einem Spiel (Karrierebestwert)"},
+    {i: 12, text: "Meiste Blocks in einem Spiel (Karrierebestwert)"},
+    
+    // absolute Kategorien
+    {i: 13, text: "Anzahl Career Games (absolut)"},
+    {i: 14, text: "Anzahl Allstar-Teilnahmen (absolut)"},
+    {i: 15, text: "MVP-Titel + All NBA Team Erscheinungen (absolut)"},
+    {i: 16, text: "DPOY-Titel + All Defensive Team Erscheinungen (absolut)"},
+
+    // Karriereschnitt
+    {i: 17, text: "Punkte pro Spiel (Karriereschnitt)"},
+    {i: 18, text: "Rebounds pro Spiel (Karriereschnitt)"},
+    {i: 19, text: "Assists pro Spiel (Karriereschnitt)"},
+    {i: 20, text: "Free Throw Percentage (Karriereschnitt)"},
+    {i: 21, text: "Field Goal Percentage (Karriereschnitt)"},
+    {i: 22, text: "3-Punkte Percentage (Karriereschnitt)"},   
+
+    // Rest
+    {i: 23, text: "Höchste Rückennummer"}
+    
 ];
 
 function getStats(){
